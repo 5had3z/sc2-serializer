a1c2b87f8714d1693f6980891d50ffeb3d85b1f5
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-11-10 16:15:15 +1100

    added db replay data viewing

--- scripts/review_resources.py
+++ scripts/review_resources.py
@@ -10,6 +10,8 @@ import typer
 from matplotlib import pyplot as plt
 from typing_extensions import Annotated
 
+import sc2_replay_reader
+
 app = typer.Typer()
 
 
@@ -28,16 +30,55 @@ class Resource:
         return cls(gid, position, qty)
 
 
-@app.command()
-def main(file: Annotated[Path, typer.Option()]):
-    """"""
-    if not file.exists():
-        raise FileNotFoundError(file)
-
+def from_experiment(file: Path):
+    """Read file generated from bin/experiment.cpp"""
     resources: list[Resource] = []
     with open(file, "r", encoding="utf-8") as f:
         while line := f.readline():
             resources.append(Resource.from_line(line))
+    return resources
+
+
+def from_database(file: Path, idx: int):
+    """Read from proper database"""
+    temp: dict[int, list[int]] = {}
+    db = sc2_replay_reader.ReplayDatabase(file)
+    replay_data = db.getEntry(idx)
+
+    for unit in replay_data.neutralUnits[0]:
+        if unit.contents > 0:
+            temp[unit.id] = []
+
+    for time_step in replay_data.neutralUnits:
+        for unit in time_step:
+            if unit.id in temp:
+                temp[unit.id].append(unit.contents)
+
+    resources: list[Resource] = []
+    for unit in replay_data.neutralUnits[0]:
+        if unit.contents > 0:
+            pos = np.array([unit.pos.x, unit.pos.y, unit.pos.z])
+            resources.append(Resource(unit.id, pos, np.array(temp[unit.id])))
+
+    return resources
+
+
+@app.command()
+def main(
+    file: Annotated[Path, typer.Option(help=".txt or .SC2Replays file to read")],
+    idx: Annotated[int, typer.Option(help="Index to sample from .SC2Replays")] = 0,
+):
+    """"""
+    if not file.exists():
+        raise FileNotFoundError(file)
+
+    match file.suffix:
+        case ".txt":
+            resources = from_experiment(file)
+        case ".SC2Replays":
+            resources = from_database(file, idx)
+        case _:
+            raise NotImplementedError(f"Can't read {file.suffix} file")
 
     assert sum([1 for r in resources if r.qty.sum() == 0]) == 0, "Uninitialized found"
 
