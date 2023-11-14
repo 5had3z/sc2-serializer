fcb3416c8ba51d37f53bdb5ddf055841e2721b35
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-11-14 16:06:05 +1100

    wip adding race-grouped upgrades

---
+++ src/sc2_replay_reader/upgrade_map.py
@@ -0,0 +1,111 @@
+"""Maps from ability id to name grouped by race"""
+
+from itertools import product
+
+from pysc2.lib.actions import FUNCTIONS
+
+
+# --- Protoss ---
+
+protos_wiki_entries = [
+    "Charge",
+    "GraviticBooster",
+    "GraviticDrive",
+    # "FluxVanes", # On wiki but not sc2api
+    "AdeptResonatingGlaives",
+    "PhoenixAnionPulseCrystals",
+    "ExtendedThermalLance",
+    "PsiStorm",
+    "Blink",
+    # "Hallucination", # On wiki but not sc2api
+    "ShadowStrike",  # On wiki as ShadowStride which is the ability name
+    "WarpGate",
+    # "TectonicDestabilizers", # On wiki but not sc2 api
+    "InterceptorGravitonCatapult",
+]
+protos_wiki_entries = [f"Research_{e}_quick" for e in protos_wiki_entries]
+
+
+def generate_protoss_research():
+    """Generate Ground|Air Armor|Weapons|Shields Level 1|2|3 for race"""
+    entries = []
+    levels = ["1", "2", "3"]
+    for a, b, c in product(["Ground", "Air"], ["Armor", "Weapons"], levels):
+        entries.append(f"Research_Protoss{a}{b}Level{c}_quick")
+    entries.extend([f"Research_ProtossShieldsLevel{l}_quick" for l in levels])
+    return entries
+
+
+protos_wiki_entries.extend(generate_protoss_research())
+# Create mapping
+protoss_id_to_str = {FUNCTIONS[p].ability_id: p for p in protos_wiki_entries}
+
+# --- Terran ---
+
+terran_wiki_entries = [
+    # "HurricaneThrusters",
+    "BansheeHyperflightRotors",
+    "SmartServos",
+    "CycloneRapidFireLaunchers",
+    # "RapidReignitionSystem",
+    # "NitroPacks",
+    "AdvancedBallistics",
+    "EnhancedShockwaves",
+    "HiSecAutoTracking",
+    # "EnhancedMunitions",
+    # "MagFieldLaunchers",
+    # "MagFieldAccelerator",
+    "RavenRecalibratedExplosives",
+    "BansheeCloakingField",
+    "ConcussiveShells",
+    "PersonalCloaking",
+    "Stimpack",
+    "BattlecruiserWeaponRefit",
+    "DrillingClaws",
+    "RavenCorvidReactor",
+    # "CaduceusReactor",
+    # "MoebiusReactor",
+    # "TransformationServos",
+    # "BehemothReactor",
+    "CombatShield",
+    "InfernalPreigniter",
+    # "NeosteelArmor",
+    "TerranStructureArmorUpgrade",
+    # "DurableMaterials",
+    "NeosteelFrame",
+]
+
+
+def generate_terran_research():
+    """Generate Ground|Air Armor|Weapons|Shields Level 1|2|3 for race"""
+    entries = []
+    levels = ["1", "2", "3"]
+    # Weapons
+    for a, c in product(["Infantry", "Vehicle", "Ship"], levels):
+        entries.append(f"Research_Terran{a}WeaponsLevel{c}_quick")
+    # Plating
+    entries.extend([f"Research_TerranInfantryArmorLevel{l}_quick" for l in levels])
+    entries.extend(
+        [f"Research_TerranVehicleAndShipPlatingLevel{l}_quick" for l in levels]
+    )
+    return entries
+
+
+terran_wiki_entries = [f"Research_{e}_quick" for e in terran_wiki_entries]
+terran_wiki_entries.extend(generate_terran_research())
+# Create mapping
+terran_id_to_str = {FUNCTIONS[p].ability_id: p for p in terran_wiki_entries}
+
+
+# --- Zerg ---
+
+
+def generate_zerg_entries():
+    return []
+
+
+zerg_wiki_entries = []
+zerg_wiki_entries = [f"Research_{e}_quick" for e in zerg_wiki_entries]
+zerg_wiki_entries.extend(generate_zerg_entries())
+# Create mapping
+protoss_id_to_str = {FUNCTIONS[p].ability_id: p for p in zerg_wiki_entries}
