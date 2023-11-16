b5e4b9e78280b4014ebdddf70983884bf0d3b923
Bryce Ferenczi <frenzi@hotmail.com.au>
2023-11-16 23:37:56 +1100

    finish remapping impl

--- src/sc2_replay_reader/upgrade_map.py
+++ src/sc2_replay_reader/upgrade_map.py
@@ -7,6 +7,11 @@ from pysc2.lib.actions import FUNCTIONS
 _levels = ["1", "2", "3"]
 
 
+def _str_tf(x):
+    """Add prefix and posfix for research action"""
+    return f"Research_{x}_quick"
+
+
 # --- Protoss ---
 def _gen_protoss():
     entries = [
@@ -28,8 +33,23 @@ def _gen_protoss():
     for a, b, c in product(["Ground", "Air"], ["Armor", "Weapons"], _levels):
         entries.append(f"Protoss{a}{b}Level{c}")
     entries.extend([f"ProtossShieldsLevel{l}" for l in _levels])
-    entries = [f"Research_{e}_quick" for e in entries]
+    entries = [_str_tf(e) for e in entries]
     return entries
+
+
+def _protoss_remap():
+    remap: dict[int, list[int]] = {}
+
+    def remap_str(base: str):
+        src = FUNCTIONS[_str_tf(base)].ability_id
+        dst = [FUNCTIONS[_str_tf(f"{base}Level{l}")].ability_id for l in _levels]
+        remap[src] = dst
+
+    for a, b in product(["Ground", "Air"], ["Armor", "Weapons"]):
+        remap_str(f"Protoss{a}{b}")
+    remap_str("ProtossShields")
+
+    return remap
 
 
 # Protoss Research ID and Name mapping
@@ -38,6 +58,8 @@ PROTOSS: dict[int, str] = {FUNCTIONS[p].ability_id: p for p in _gen_protoss()}
 PROTOSS_IDX: dict[int, int] = {
     _id: idx for idx, _id in enumerate(sorted(PROTOSS.keys()))
 }
+# Protoss remap non-leveled action to leveled
+PROTOSS_REMAP: dict[int, list[int]] = _protoss_remap()
 
 
 # --- Terran ---
@@ -81,32 +103,30 @@ def _gen_terran():
     # Plating
     entries.extend([f"TerranInfantryArmorLevel{l}" for l in _levels])
     entries.extend([f"TerranVehicleAndShipPlatingLevel{l}" for l in _levels])
-
-    entries = [f"Research_{e}_quick" for e in entries]
+    entries = [_str_tf(e) for e in entries]
     return entries
-
-
-# Terran Research ID and Name mapping
-TERRAN: dict[int, str] = {FUNCTIONS[p].ability_id: p for p in _gen_terran()}
-# Terran Research ID to State Feature Index Mapping
-TERRAN_IDX: dict[int, int] = {_id: idx for idx, _id in enumerate(sorted(TERRAN.keys()))}
 
 
 def _terran_remap():
     remap: dict[int, list[int]] = {}
 
     def remap_str(base: str):
-        ext = lambda x: f"Research_{x}_quick"
-        src = FUNCTIONS[ext(base)].ability_id
-        dst = [FUNCTIONS[ext(f"{base}Level{l}")].ability_id for l in _levels]
+        src = FUNCTIONS[_str_tf(base)].ability_id
+        dst = [FUNCTIONS[_str_tf(f"{base}Level{l}")].ability_id for l in _levels]
         remap[src] = dst
 
     for a in ["Infantry", "Vehicle", "Ship"]:
         remap_str(f"Terran{a}Weapons")
+    remap_str("TerranInfantryArmor")
+    remap_str("TerranVehicleAndShipPlating")
 
     return remap
 
 
+# Terran Research ID and Name mapping
+TERRAN: dict[int, str] = {FUNCTIONS[p].ability_id: p for p in _gen_terran()}
+# Terran Research ID to State Feature Index Mapping
+TERRAN_IDX: dict[int, int] = {_id: idx for idx, _id in enumerate(sorted(TERRAN.keys()))}
 # Terran remap non-leveled action to leveled
 TERRAN_REMAP: dict[int, list[int]] = _terran_remap()
 
@@ -138,15 +158,33 @@ def _gen_zerg():
     for a, l in product(["Melee", "Missile"], _levels):
         entries.append(f"Zerg{a}WeaponsLevel{l}")
     entries.extend([f"ZergFlyerAttackLevel{l}" for l in _levels])
-
     # Ground and flyer armor
     for a, l in product(["Ground", "Flyer"], _levels):
         entries.append(f"Zerg{a}ArmorLevel{l}")
-    entries = [f"Research_{e}_quick" for e in entries]
+    entries = [_str_tf(e) for e in entries]
     return entries
+
+
+def _zerg_remap():
+    remap: dict[int, list[int]] = {}
+
+    def remap_str(base: str):
+        src = FUNCTIONS[_str_tf(base)].ability_id
+        dst = [FUNCTIONS[_str_tf(f"{base}Level{l}")].ability_id for l in _levels]
+        remap[src] = dst
+
+    for a in ["Melee", "Missile"]:
+        remap_str(f"Zerg{a}Weapons")
+    remap_str("ZergFlyerAttack")
+    for a in ["Ground", "Flyer"]:
+        remap_str(f"Zerg{a}Armor")
+
+    return remap
 
 
 # Zerg Research ID and Name mapping
 ZERG: dict[int, str] = {FUNCTIONS[p].ability_id: p for p in _gen_zerg()}
 # Zerg Research ID to State Feature Index Mapping
 ZERG_IDX: dict[int, int] = {_id: idx for idx, _id in enumerate(sorted(ZERG.keys()))}
+# Zerg remap non-leveled action to leveled
+ZERG_REMAP: dict[int, list[int]] = _zerg_remap()
