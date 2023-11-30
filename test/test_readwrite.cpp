1bf72beca1cd441fc7098eb0c23b07ed485f778c
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-11-30 12:56:25 +1100

    fix testing to reflect new ordering

--- test/test_readwrite.cpp
+++ test/test_readwrite.cpp
@@ -207,23 +207,22 @@ TEST(AutoVectorTest, NeutralUnit)
     {
         const auto vec = cvt::vectorize<float>(unit, false);
         ASSERT_EQ(static_cast<float>(unit.id), vec[0]);
-        ASSERT_EQ(static_cast<float>(unit.observation), vec[2]);
-        ASSERT_EQ(static_cast<float>(unit.health), vec[3]);
-        ASSERT_EQ(static_cast<float>(unit.pos.x), vec[5]);
-        ASSERT_EQ(static_cast<float>(unit.pos.y), vec[6]);
-        ASSERT_EQ(static_cast<float>(unit.pos.z), vec[7]);
+        ASSERT_EQ(static_cast<float>(unit.health), vec[2]);
+        ASSERT_EQ(static_cast<float>(unit.pos.x), vec[4]);
+        ASSERT_EQ(static_cast<float>(unit.pos.y), vec[5]);
+        ASSERT_EQ(static_cast<float>(unit.pos.z), vec[6]);
+        ASSERT_EQ(static_cast<float>(unit.observation), vec[10]);
     }
     {
         const auto vec = cvt::vectorize<float>(unit, true);
-        const int off = 2;// offset for observation unpack
         ASSERT_EQ(static_cast<float>(unit.id), vec[0]);
-        ASSERT_EQ(0.f, vec[2]);// visible
-        ASSERT_EQ(1.f, vec[3]);// snapshot
-        ASSERT_EQ(0.f, vec[4]);// hidden
-        ASSERT_EQ(static_cast<float>(unit.health), vec[3 + off]);
-        ASSERT_EQ(static_cast<float>(unit.pos.x), vec[5 + off]);
-        ASSERT_EQ(static_cast<float>(unit.pos.y), vec[6 + off]);
-        ASSERT_EQ(static_cast<float>(unit.pos.z), vec[7 + off]);
+        ASSERT_EQ(static_cast<float>(unit.health), vec[2]);
+        ASSERT_EQ(static_cast<float>(unit.pos.x), vec[4]);
+        ASSERT_EQ(static_cast<float>(unit.pos.y), vec[5]);
+        ASSERT_EQ(static_cast<float>(unit.pos.z), vec[6]);
+        ASSERT_EQ(0.f, vec[10]);// visible
+        ASSERT_EQ(1.f, vec[11]);// snapshot
+        ASSERT_EQ(0.f, vec[12]);// hidden
     }
 }
 
@@ -239,24 +238,26 @@ TEST(AutoVectorTest, Unit)
     unit.heading = 1.3f;
     unit.build_progress = 1.f;
     unit.is_flying = true;
+
     {
         const auto vec = cvt::vectorize<float>(unit, false);
         ASSERT_EQ(unit.id, vec[0]);
         ASSERT_EQ(static_cast<float>(unit.alliance), vec[3]);
         ASSERT_EQ(static_cast<float>(unit.cloak_state), vec[4]);
-        ASSERT_EQ(unit.energy, vec[10]);
-        ASSERT_EQ(unit.pos.x, vec[17]);
-        ASSERT_EQ(unit.pos.y, vec[18]);
-        ASSERT_EQ(unit.pos.z, vec[19]);
-        ASSERT_EQ(unit.heading, vec[20]);
-        ASSERT_EQ(unit.build_progress, vec[22]);
-        ASSERT_EQ(unit.is_flying, vec[24]);
+        ASSERT_EQ(unit.energy, vec[11]);
+        ASSERT_EQ(unit.pos.x, vec[16]);
+        ASSERT_EQ(unit.pos.y, vec[17]);
+        ASSERT_EQ(unit.pos.z, vec[18]);
+        ASSERT_EQ(unit.heading, vec[19]);
+        ASSERT_EQ(unit.build_progress, vec[21]);
+        ASSERT_EQ(unit.is_flying, vec[27]);
     }
     {
         const auto vec = cvt::vectorize<float>(unit, true);
         const int obsOff = 2;
         const int aliOff = 3 + obsOff;
         const int clkOff = 4 + aliOff;
+        const int addOff = 2 + clkOff;
         ASSERT_EQ(unit.id, vec[0]);
         // alliance
         ASSERT_EQ(1.f, vec[3 + obsOff + 0]);// self
@@ -269,12 +270,12 @@ TEST(AutoVectorTest, Unit)
         ASSERT_EQ(1.f, vec[4 + aliOff + 2]);// detected
         ASSERT_EQ(0.f, vec[4 + aliOff + 3]);// uncloaked
         ASSERT_EQ(0.f, vec[4 + aliOff + 4]);// allied
-        ASSERT_EQ(unit.energy, vec[10 + clkOff]);
-        ASSERT_EQ(unit.pos.x, vec[17 + clkOff]);
-        ASSERT_EQ(unit.pos.y, vec[18 + clkOff]);
-        ASSERT_EQ(unit.pos.z, vec[19 + clkOff]);
-        ASSERT_EQ(unit.heading, vec[20 + clkOff]);
-        ASSERT_EQ(unit.build_progress, vec[22 + clkOff]);
-        ASSERT_EQ(unit.is_flying, vec[24 + clkOff]);
+        ASSERT_EQ(unit.energy, vec[11 + addOff]);
+        ASSERT_EQ(unit.pos.x, vec[16 + addOff]);
+        ASSERT_EQ(unit.pos.y, vec[17 + addOff]);
+        ASSERT_EQ(unit.pos.z, vec[18 + addOff]);
+        ASSERT_EQ(unit.heading, vec[19 + addOff]);
+        ASSERT_EQ(unit.build_progress, vec[21 + addOff]);
+        ASSERT_EQ(unit.is_flying, vec[27 + addOff]);
     }
 }
