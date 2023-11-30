70dff3a17fecf53ccff464048bbb9404c1c898cb
Bryce Ferenczi <frenzi@hotmail.com.au>
2023-11-30 13:11:45 +1100

    bump s2client-api version to address missing vespene

--- src/sc2_replay_reader/__init__.py
+++ src/sc2_replay_reader/__init__.py
@@ -1,5 +1,5 @@
-from ._sc2_replay_reader import *  # noqa : F403
-
 from pathlib import Path
+
+from ._sc2_replay_reader import *  # noqa : F403
 
 GAME_INFO_FILE = (Path(__file__).parent / "game_info.yaml").absolute()
