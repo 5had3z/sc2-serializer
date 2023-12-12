dbd14c4e0d9ccfce085e7f6c5c52b02ad2476e0f
taco <renew3@student.monash.edu>
2023-12-12 14:58:56 +1100

    add database making

---
+++ scripts/gen_database.py
@@ -0,0 +1,109 @@
+import sqlite3
+from summaryStats import SC2Replay, LambdaFunctionType
+import os
+from pathlib import Path
+from torch.utils.data import DataLoader
+import torch
+from sc2_replay_reader import Score
+from typing import Dict, Tuple
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer()
+
+
+def make_database(path, additional_columns, features, lambda_columns):
+    # Connect to the SQLite database (creates a new database if it doesn't exist)
+    conn = sqlite3.connect(str(path))
+
+    # Create a cursor object to execute SQL commands
+    cursor = conn.cursor()
+
+    # Create a table with the specified headings and data types
+    create_table_sql = f"""
+        CREATE TABLE game_data (
+            {', '.join(f"{column} {datatype}" for column, datatype in additional_columns.items())},
+            {', '.join(f"{column} {datatype}" for column, datatype in features.items())},
+            {', '.join(f"{column} {datatype}" for column, (datatype, _) in lambda_columns.items())}
+
+        )
+    """
+    cursor.execute(create_table_sql)
+    return conn, cursor
+
+
+def close_database(conn):
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+
+def add_to_database(cursor, data_dict):
+    columns = ", ".join(data_dict.keys())
+    placeholders = ", ".join("?" for _ in data_dict.values())
+
+    query = f"""
+        INSERT INTO game_data ({columns})
+        VALUES ({placeholders})
+    """
+
+    cursor.execute(query, tuple(data_dict.values()))
+
+
+@app.command()
+def main(
+    workspace: Annotated[Path, typer.Option()], workers: Annotated[int, typer.Option()]
+):
+    features = {
+        "replayHash": "TEXT",
+        "gameVersion": "TEXT",
+        "playerId": "INTEGER",
+        "playerRace": "TEXT",
+        "playerResult": "TEXT",
+        "playerMMR": "INTEGER",
+        "playerAPM": "INTEGER",
+    }
+    # Manually include additional columns
+    additional_columns = {"partition": "TEXT", "idx": "INTEGER"}
+    all_attributes = [
+        attr
+        for attr in dir(Score)
+        if not callable(getattr(Score, attr))
+        if "__" not in attr
+    ]
+
+    lambda_columns: Dict[str, Tuple[str, LambdaFunctionType]] = {
+        "max_units": ("TEXT", lambda y: max(len(x) for x in y.data.units)),
+        **{
+            f"final_{i}": (
+                "FLOAT",
+                (lambda k: lambda y, key=k: float(getattr(y.data.score[-1], key)))(i),
+            )
+            for i in all_attributes
+        },
+    }
+
+    conn, cursor = make_database(
+        workspace / "gamedata.db", additional_columns, features, lambda_columns
+    )
+
+    dataset = SC2Replay(
+        Path(os.environ["DATAPATH"]), set(features.keys()), lambda_columns
+    )
+    dataloader = DataLoader(dataset, num_workers=workers, batch_size=1)
+    for idx, d in enumerate(dataloader):
+        converted_d = {}
+
+        for key, value in d.items():
+            if isinstance(value, torch.Tensor):
+                converted_d[key] = value[0].item()
+            elif isinstance(value, str):
+                converted_d[key] = value[0]
+        if idx % 5 == 0:
+            conn.commit()
+
+        add_to_database(cursor, converted_d)
+
+
+if __name__ == "__main__":
+    app()
---
+++ scripts/summaryStats.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+from typing import Dict, Tuple, Callable
+import torch
+from sc2_replay_reader import (
+    GAME_INFO_FILE,
+    ReplayDatabase,
+    ReplayParser,
+    setReplayDBLoggingLevel,
+    spdlog_lvl,
+)
+from torch.utils.data import Dataset
+
+from utils import upper_bound
+
+ENUM_KEYS = {"playerRace", "playerResult"}
+LambdaFunctionType = Callable[[ReplayParser], float | int]
+
+
+class SC2Replay(Dataset):
+    def __init__(
+        self,
+        basepath: Path,
+        features: set[str],
+        lambda_columns: Dict[str, Tuple[str, LambdaFunctionType]],
+    ) -> None:
+        super().__init__()
+        self.features = features
+        self.db_handle = ReplayDatabase()
+        self.parser = ReplayParser(GAME_INFO_FILE)
+        self.lambda_columns = lambda_columns
+
+        setReplayDBLoggingLevel(spdlog_lvl.warn)
+
+        if basepath.is_file():
+            self.replays = [basepath]
+        else:
+            self.replays = list(basepath.glob("*.SC2Replays"))
+            assert len(self.replays) > 0, f"No .SC2Replays found in {basepath}"
+
+        replays_per_file = torch.empty([len(self.replays) + 1], dtype=torch.int)
+        replays_per_file[0] = 0
+        for idx, replay in enumerate(self.replays, start=1):
+            self.db_handle.open(replay)
+            replays_per_file[idx] = self.db_handle.size()
+
+        self._accumulated_replays = torch.cumsum(replays_per_file, 0)
+        self.n_replays = int(self._accumulated_replays[-1].item())
+        assert self.n_replays > 0, "No replays in dataset"
+
+    def __len__(self) -> int:
+        return self.n_replays
+
+    def __getitem__(self, index: int):
+        file_index = upper_bound(self._accumulated_replays, index)
+        self.db_handle.open(self.replays[file_index])
+        db_index = index - int(self._accumulated_replays[file_index].item())
+        assert (  # This should hold if calculation checks out
+            db_index < self.db_handle.size()
+        ), f"{db_index} exceeds {self.db_handle.size()}"
+
+        self.parser.parse_replay(self.db_handle.getEntry(db_index))
+        data = {p: getattr(self.parser.data, p, None) for p in self.features}
+        data = {k: int(v) if k in ENUM_KEYS else v for k, v in data.items()}
+
+        for k, (_, f) in self.lambda_columns.items():
+            data[k] = f(self.parser)
+
+        return {
+            "partition": str(self.replays[file_index].name),
+            "idx": db_index,
+            **data,
+        }
+
+    def __iter__(self):
+        for index in range(len(self)):
+            yield self[index]
