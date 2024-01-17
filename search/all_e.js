e6b315deb2644ea27a7c913c1f1a5868b7900265
5had3z <59598353+5had3z@users.noreply.github.com>
2024-01-17 03:37:26 +0000

    Deploying to gh-pages from  @ a7cfe7c5b46541007e26627883b4bd999958de6f 🚀

--- search/all_e.js
+++ search/all_e.js
@@ -5,10 +5,6 @@ var searchData=
   ['setstride_2',['SetStride',['../d5/db8/classcvt_1_1_strided_converter.html#ace870b8744fc74b36a8228045cb24e52',1,'cvt::StridedConverter']]],
   ['size_3',['size',['../da/da1/classcvt_1_1_replay_database.html#a4d420282a97ce646a6f5de280dc770f2',1,'cvt::ReplayDatabase']]],
   ['stepdata_4',['StepData',['../d1/df0/structcvt_1_1_step_data.html',1,'cvt']]],
-  ['stepdatanounits_5',['StepDataNoUnits',['../d9/d21/structcvt_1_1_step_data_no_units.html',1,'cvt']]],
-  ['stepdatanounitsminimap_6',['StepDataNoUnitsMiniMap',['../d9/dfa/structcvt_1_1_step_data_no_units_mini_map.html',1,'cvt']]],
-  ['stepdatasoa_7',['StepDataSoA',['../df/d16/structcvt_1_1_step_data_so_a.html',1,'cvt']]],
-  ['stepdatasoanounits_8',['StepDataSoANoUnits',['../d6/d11/structcvt_1_1_step_data_so_a_no_units.html',1,'cvt']]],
-  ['stepdatasoanounitsminimap_9',['StepDataSoANoUnitsMiniMap',['../d4/d93/structcvt_1_1_step_data_so_a_no_units_mini_map.html',1,'cvt']]],
-  ['stridedconverter_10',['StridedConverter',['../d5/db8/classcvt_1_1_strided_converter.html',1,'cvt']]]
+  ['stepdatasoa_5',['StepDataSoA',['../df/d16/structcvt_1_1_step_data_so_a.html',1,'cvt']]],
+  ['stridedconverter_6',['StridedConverter',['../d5/db8/classcvt_1_1_strided_converter.html',1,'cvt']]]
 ];
--- search/classes_8.js
+++ search/classes_8.js
@@ -1,13 +1,12 @@
 var searchData=
 [
   ['replaydata_0',['ReplayData',['../df/d6f/structcvt_1_1_replay_data.html',1,'cvt']]],
-  ['replaydatabase_1',['ReplayDatabase',['../da/da1/classcvt_1_1_replay_database.html',1,'cvt']]],
-  ['replaydatabase_3c_20datasoa_20_3e_2',['ReplayDatabase&lt; DataSoA &gt;',['../da/da1/classcvt_1_1_replay_database.html',1,'cvt']]],
-  ['replaydatasoa_3',['ReplayDataSoA',['../da/d56/structcvt_1_1_replay_data_so_a.html',1,'cvt']]],
-  ['replaydatatemplate_4',['ReplayDataTemplate',['../d4/d5b/structcvt_1_1_replay_data_template.html',1,'cvt']]],
-  ['replaydatatemplatesoa_5',['ReplayDataTemplateSoA',['../df/dd2/structcvt_1_1_replay_data_template_so_a.html',1,'cvt']]],
-  ['replaydatatemplatesoa_3c_20stepdatasoa_20_3e_6',['ReplayDataTemplateSoA&lt; StepDataSoA &gt;',['../df/dd2/structcvt_1_1_replay_data_template_so_a.html',1,'cvt']]],
-  ['replayinfo_7',['ReplayInfo',['../db/df5/structcvt_1_1_replay_info.html',1,'cvt']]],
-  ['replayparser_8',['ReplayParser',['../db/d4a/classcvt_1_1_replay_parser.html',1,'cvt']]],
-  ['resourceobs_9',['ResourceObs',['../d0/da0/structcvt_1_1_resource_obs.html',1,'cvt']]]
+  ['replaydata2_1',['ReplayData2',['../d7/d38/structcvt_1_1_replay_data2.html',1,'cvt']]],
+  ['replaydata2soa_2',['ReplayData2SoA',['../d4/dff/structcvt_1_1_replay_data2_so_a.html',1,'cvt']]],
+  ['replaydatabase_3',['ReplayDatabase',['../da/da1/classcvt_1_1_replay_database.html',1,'cvt']]],
+  ['replaydatabase_3c_20datasoa_20_3e_4',['ReplayDatabase&lt; DataSoA &gt;',['../da/da1/classcvt_1_1_replay_database.html',1,'cvt']]],
+  ['replaydatasoa_5',['ReplayDataSoA',['../da/d56/structcvt_1_1_replay_data_so_a.html',1,'cvt']]],
+  ['replayinfo_6',['ReplayInfo',['../db/df5/structcvt_1_1_replay_info.html',1,'cvt']]],
+  ['replayparser_7',['ReplayParser',['../db/d4a/classcvt_1_1_replay_parser.html',1,'cvt']]],
+  ['resourceobs_8',['ResourceObs',['../d0/da0/structcvt_1_1_resource_obs.html',1,'cvt']]]
 ];
