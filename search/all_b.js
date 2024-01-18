cd8da7f6bbaf33295b5549ad98ffea16e0e7bd3e
5had3z <59598353+5had3z@users.noreply.github.com>
2024-01-18 04:50:38 +0000

    Deploying to gh-pages from  @ 98afab5a45e0ad512c233c791e6302c18ca741f6 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['ongameend_0',['OnGameEnd',['../d8/d39/classcvt_1_1_base_converter.html#a3b7164d8368b3298aeba8f70ec28effe',1,'cvt::BaseConverter']]],
-  ['ongamestart_1',['ongamestart',['../d8/d39/classcvt_1_1_base_converter.html#a0153640dd6d53ec079cef40f1c08263a',1,'cvt::BaseConverter::OnGameStart()'],['../d5/db8/classcvt_1_1_strided_converter.html#aba2f60a6b05cc7f75b621ca73714948a',1,'cvt::StridedConverter::OnGameStart()']]],
+  ['ongamestart_1',['OnGameStart',['../d8/d39/classcvt_1_1_base_converter.html#a0153640dd6d53ec079cef40f1c08263a',1,'cvt::BaseConverter::OnGameStart()'],['../d5/db8/classcvt_1_1_strided_converter.html#aba2f60a6b05cc7f75b621ca73714948a',1,'cvt::StridedConverter::OnGameStart()']]],
   ['open_2',['open',['../da/da1/classcvt_1_1_replay_database.html#a06f8646d61cc16572fa35cf7cce7ed71',1,'cvt::ReplayDatabase']]],
   ['operator_28_29_3',['operator()',['../dc/dcb/structcvt_1_1_caster.html#a4f59d7b51bcc0ccecfe6c988672641cb',1,'cvt::Caster']]]
 ];
