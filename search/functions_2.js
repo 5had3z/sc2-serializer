76a91b69a9c603453b548ae5ee6df1a6a4bedaa5
5had3z <59598353+5had3z@users.noreply.github.com>
2024-03-18 01:09:23 +0000

    Deploying to gh-pages from  @ b09b18fa04d6cc02a055efcff04f1c6c9f9cbbbe 🚀

--- search/functions_2.js
+++ search/functions_2.js
@@ -5,5 +5,7 @@ var searchData=
   ['gethashid_2',['getHashId',['../da/da1/classcvt_1_1_replay_database.html#a0fc0bf1cde71931bfd902860e0c862d8',1,'cvt::ReplayDatabase']]],
   ['gethashidimpl_3',['getHashIdImpl',['../d2/d1e/structcvt_1_1_database_interface.html#a4b85e822907a4ca9cb021bab0a97da3e',1,'cvt::DatabaseInterface']]],
   ['getheader_4',['getHeader',['../da/da1/classcvt_1_1_replay_database.html#a3c0194920882cbc160245e5a80187259',1,'cvt::ReplayDatabase']]],
-  ['getstride_5',['GetStride',['../d5/db8/classcvt_1_1_strided_converter.html#a096cf3cf734d26a6d8b7014d2f83cfec',1,'cvt::StridedConverter']]]
+  ['getminimapfeatures_5',['getMinimapFeatures',['../db/d4a/classcvt_1_1_replay_parser.html#a68a9e73756a66ebd17cba748c2abf285',1,'cvt::ReplayParser']]],
+  ['getplayerminimapexpansion_6',['getPlayerMinimapExpansion',['../db/d4a/classcvt_1_1_replay_parser.html#acdd9304b1574c920f99f81202eb70ef0',1,'cvt::ReplayParser']]],
+  ['getstride_7',['GetStride',['../d5/db8/classcvt_1_1_strided_converter.html#a096cf3cf734d26a6d8b7014d2f83cfec',1,'cvt::StridedConverter']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['setactionsaving_0',['SetActionSaving',['../d5/db8/classcvt_1_1_strided_converter.html#a882c66eb686d4d695d336363866c461b',1,'cvt::StridedConverter']]],
   ['setminimapfeatures_1',['setMinimapFeatures',['../db/d4a/classcvt_1_1_replay_parser.html#a8dfd86ce72356cd302ff628c0683b8ff',1,'cvt::ReplayParser']]],
-  ['setreplayinfo_2',['setReplayInfo',['../d8/d39/classcvt_1_1_base_converter.html#a72fad2ae1125761bdff88adef451f6e3',1,'cvt::BaseConverter']]],
-  ['setstride_3',['SetStride',['../d5/db8/classcvt_1_1_strided_converter.html#ace870b8744fc74b36a8228045cb24e52',1,'cvt::StridedConverter']]],
-  ['size_4',['size',['../da/da1/classcvt_1_1_replay_database.html#a4d420282a97ce646a6f5de280dc770f2',1,'cvt::ReplayDatabase']]]
+  ['setplayerminimapexpansion_2',['setPlayerMinimapExpansion',['../db/d4a/classcvt_1_1_replay_parser.html#a2966c4b90913213e8b829cab75b0df96',1,'cvt::ReplayParser']]],
+  ['setreplayinfo_3',['setReplayInfo',['../d8/d39/classcvt_1_1_base_converter.html#a72fad2ae1125761bdff88adef451f6e3',1,'cvt::BaseConverter']]],
+  ['setstride_4',['SetStride',['../d5/db8/classcvt_1_1_strided_converter.html#ace870b8744fc74b36a8228045cb24e52',1,'cvt::StridedConverter']]],
+  ['size_5',['size',['../da/da1/classcvt_1_1_replay_database.html#a4d420282a97ce646a6f5de280dc770f2',1,'cvt::ReplayDatabase']]]
 ];
