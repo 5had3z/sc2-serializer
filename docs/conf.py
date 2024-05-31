bf0c8b04e723bda2354960cb68166bdcb68759f1
Bryce Ferenczi <frenzi@hotmail.com.au>
2024-05-31 10:50:36 +1000

    folder needs to be in html...
    Signed-off-by: Bryce Ferenczi <frenzi@hotmail.com.au>

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,9 @@ import sys
 import subprocess
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent / "_build" / "sc2_replay_reader"))
+sys.path.insert(
+    0, str(Path(__file__).parent / "_build" / "html" / "_sources" / "sc2_replay_reader")
+)
 
 # Doxygen
 subprocess.call("doxygen Doxyfile", shell=True)
